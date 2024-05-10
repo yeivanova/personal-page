51705cc4f3488b025db9d10cc8668ba872f52cf2
yeivanova <yeivanova@gmail.com>
2024-05-10 15:04:13 +0200

    updated layout

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { FC, useState, useEffect } from "react";
 import { Header } from "../header/header";
 import { Bento } from "../bento/bento";
 import { ProjectsSlider } from "../projectsSlider/projectsSlider";
@@ -10,12 +10,32 @@ import tag from "../../images/tag@2x.png";
 import arrow from "../../images/arrow@2x.png";
 
 export const App: FC = () => {
+    const [isMobile, setIsMobile] = useState<boolean>(window.innerWidth < 768 ? true : false);
+    const [windowDimension, setWindowDimension] = useState<number | null>(null);
+
+    useEffect(() => {
+        setWindowDimension(window.innerWidth);
+        windowDimension && windowDimension < 768 ? setIsMobile(true) : setIsMobile(false);
+    }, [windowDimension, isMobile]);
+
+    useEffect(() => {
+        function handleWindowResize() {
+            setWindowDimension(window.innerWidth);
+        }
+
+        window.addEventListener("resize", handleWindowResize);
+
+        return () => {
+            window.removeEventListener("resize", handleWindowResize);
+        };
+    }, []);
+
     return (
         <div className={styles.app}>
             <Header />
             <main className={styles.main} id="wrapper">
-                <div className={styles.container}>
-                    <section className={cn(styles.col, styles.full_width, styles.col_about)} id="about">
+                <section className={styles.section} id="about">
+                    <div className={cn(styles.col, styles.full_width)}>
                         <Bento>
                             <h1>
                                 <span className="showup">Hey!&nbsp;</span>{" "}
@@ -32,37 +52,46 @@ export const App: FC = () => {
                                 <br />I get great <span className={cn("underlined", "underlined2")}>inspiration</span> from modern web design and I feel excitement when starting to work with a new design markup 🌟
                             </p>
                         </Bento>
-                    </section>
-                    <div className={cn(styles.col, styles.col_aside, styles.col_button)}>
+                    </div>
+                    <div className={cn(styles.col, styles.col_aside)}>
                         <a className={styles.vertical_link} href="https://t.me/yeivanova" target="_blank" rel="noreferrer">
                             ⚡️ &nbsp;Send me a message&nbsp; ⚡️
                         </a>
                     </div>
-                    <div className={cn(styles.col, styles.col_1, styles.col_stack)}>
+                </section>
+                <section className={styles.section} id="stack">
+                    <div className={cn(styles.col, styles.col_1)}>
                         <h2 className={"align-center"}>My stack</h2>
                         <ScrollList list={["HTML & CSS", "LESS/SCSS/Stylus", "JavaScript, TypeScript", "React JS, Redux", "Jest, Cypress", "Gulp, Webpack", "Git", "Figma"]} />
                     </div>
-                    <div className={cn(styles.col, styles.col_2, styles.col_tag_img, styles.center_wrapper)}>
+                    <div className={cn(styles.col, styles.col_2, styles.center_wrapper)}>
                         <img className={styles.tag_img} src={tag} alt="" />
                     </div>
-                    <div className={cn(styles.col, styles.col_aside, styles.sticky)}>
-                        <SocialLinks />
-                    </div>
-                    <section className={cn(styles.col, styles.full_width, styles.col_projects)} id="projects">
+                    {!isMobile && (
+                        <div className={cn(styles.col, styles.col_aside, styles.sticky)}>
+                            <SocialLinks />
+                        </div>
+                    )}
+                    <div className={cn(styles.col, styles.full_width, styles.col_projects_header)}>
+                        {" "}
                         <h2>
                             Here is a list of my open source projects,
-                            <br /> web apps and experiments</h2>
-                    </section>
-                </div>
-                <section className={cn(styles.col, styles.full_width, styles.col_projects)} id="projects">
-                    <ProjectsSlider />
+                            <br /> web apps and experiments
+                        </h2>
+                    </div>
                 </section>
 
-                <div className={styles.container}>
-                    <section className={cn(styles.col, styles.col_1, styles.col_arrow_img)} id="contacts">
+                <section id="projects" className={styles.col_projects}>
+                    <div className={cn(styles.col, styles.full_width)}>
+                        <ProjectsSlider />
+                    </div>
+                </section>
+
+                <section className={cn(styles.section, styles.section_contacts)} id="contacts">
+                    <div className={cn(styles.col, styles.col_1)} id="contacts">
                         <img className={styles.arrow_img} src={arrow} alt="" />
-                    </section>
-                    <div className={cn(styles.col, styles.col_2, styles.center_wrapper, styles.col_contacts)}>
+                    </div>
+                    <div className={cn(styles.col, styles.col_2, styles.center_wrapper)}>
                         <div className={styles.text_wrapper}>
                             <h3>Send me a&nbsp;massage</h3>
                             <p>
@@ -74,7 +103,12 @@ export const App: FC = () => {
                             </p>
                         </div>
                     </div>
-                </div>
+                </section>
+                {isMobile && (
+                    <div className={cn(styles.col, styles.col_aside, styles.sticky)}>
+                        <SocialLinks />
+                    </div>
+                )}
             </main>
         </div>
     );
